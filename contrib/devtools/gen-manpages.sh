bde06fa419c2e595c613c5945ed007fc754fc5d0
CrimsonDev14 <30295798+CrimsonDev14@users.noreply.github.com>
2017-07-20 00:36:03 +0700

    Add files via upload

--- contrib/devtools/gen-manpages.sh
+++ contrib/devtools/gen-manpages.sh
@@ -12,7 +12,7 @@ crimsonQT=${crimsonQT:-$SRCDIR/qt/crimson-qt}
 [ ! -x $crimsonD ] && echo "$crimsonD not found or not executable." && exit 1
 
 # The autodetected version git tag can screw up manpage output a little bit
-DGBVER=($($crimsonCLI --version | head -n1 | awk -F'[ -]' '{ print $6, $7 }'))
+CRMVER=($($crimsonCLI --version | head -n1 | awk -F'[ -]' '{ print $6, $7 }'))
 
 # Create a footer file with copyright content.
 # This gets autodetected fine for crimsond if --version-string is not set,
@@ -22,8 +22,8 @@ $crimsonD --version | sed -n '1!p' >> footer.h2m
 
 for cmd in $crimsonD $crimsonCLI $crimsonTX $crimsonQT; do
   cmdname="${cmd##*/}"
-  help2man -N --version-string=${DGBVER[0]} --include=footer.h2m -o ${MANDIR}/${cmdname}.1 ${cmd}
-  sed -i "s/\\\-${DGBVER[1]}//g" ${MANDIR}/${cmdname}.1
+  help2man -N --version-string=${CRMVER[0]} --include=footer.h2m -o ${MANDIR}/${cmdname}.1 ${cmd}
+  sed -i "s/\\\-${CRMVER[1]}//g" ${MANDIR}/${cmdname}.1
 done
 
 rm -f footer.h2m
